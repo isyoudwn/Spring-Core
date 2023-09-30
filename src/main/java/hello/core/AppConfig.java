d0a24805e6e7816350e4cad385f333dda6fa6bd6
임채연 <dlacodus0407@gmail.com>
2023-09-30 18:32:22 +0900

    Section3 : 스프링으로 전환

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,21 +8,35 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+
+/**
+ * @Configuration : 설정 정보(구성 정보)
+ * application이 어떻게 구성되는지 설정정보를 표현할 때 적어준다.
+ *
+ * 각 메소드에 @Bean을 적어서 메소드르 스프링 컨테이너에 등록한다.
+ */
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,11 +3,31 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        /**
+         * ApplicationContext는 스프링 컨테이너임 -> 모든 @Bean을 관리
+         * ApplicationContext를 생성하기 위해서는 new AnnotationConfigApplicationContext(); 사용
+         * parameter로 환경설정 정보를 넣어줌. (AppConfig)
+         * AppConfig에 있는 환경설정 정보들을 스프링컨테이너에 넣어줌.
+         **/
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        /**
+         * getBean으로 객체를 찾아 옴. 이때 객체(빈)의 이름은 메소드의 이름과 동일함.
+         * getBean의 두 번째 파라미터는 타입을 의미.
+         * 즉 기존에는 AppConfing에서 직접 객체를 조회하였지만, 스프링 컨테이너를 통해서 빈(객체)를 조회하여 필요한 객체를 찾아야 함
+         **/
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+
         Member member = new Member(1L, "WOODZ", Grade.VIP);
 
         memberService.join(member);
